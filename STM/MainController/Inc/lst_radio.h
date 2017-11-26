aff4a017df9428283b5fe60ba9f0d27c4c48b2b0
nmartont <nmarton.t@gmail.com>
2017-11-26 23:06:25 +0100

    Added UART1 radio support for the starter thing because it's very easy

--- STM/MainController/Inc/lst_radio.h
+++ STM/MainController/Inc/lst_radio.h
@@ -14,8 +14,18 @@
 #include "stm32f4xx_it.h"
 #include "cmsis_os.h"
 #include "main.h"
+#include "lst_uart.h"
 
 /* Defines -------------------------------------------------------------------*/
+#define LST_RADIO_MSG_5 '5'
+#define LST_RADIO_MSG_4 '4'
+#define LST_RADIO_MSG_3 '3'
+#define LST_RADIO_MSG_2 '2'
+#define LST_RADIO_MSG_1 '1'
+#define LST_RADIO_MSG_0 '0'
+
+#define LST_RADIO_MSG_NOT_RECEIVED	0
+#define LST_RADIO_MSG_RECEIVED 			1
 
 /* Function prototypes -------------------------------------------------------*/
 void LST_Radio_Init();
@@ -24,5 +34,11 @@ void LST_Radio_Process_Incoming_Byte();
 /* Private variables ---------------------------------------------------------*/
 
 /* External variables --------------------------------------------------------*/
+extern uint8_t lst_radio_msg_5_received;
+extern uint8_t lst_radio_msg_4_received;
+extern uint8_t lst_radio_msg_3_received;
+extern uint8_t lst_radio_msg_2_received;
+extern uint8_t lst_radio_msg_1_received;
+extern uint8_t lst_radio_msg_0_received;
 
 #endif /* LST_RADIO_H_ */
--- STM/MainController/Src/lst_radio.c
+++ STM/MainController/Src/lst_radio.c
@@ -9,6 +9,12 @@
 #include "lst_radio.h"
 
 /* Private define ------------------------------------------------------------*/
+uint8_t lst_radio_msg_5_received = LST_RADIO_MSG_NOT_RECEIVED;
+uint8_t lst_radio_msg_4_received = LST_RADIO_MSG_NOT_RECEIVED;
+uint8_t lst_radio_msg_3_received = LST_RADIO_MSG_NOT_RECEIVED;
+uint8_t lst_radio_msg_2_received = LST_RADIO_MSG_NOT_RECEIVED;
+uint8_t lst_radio_msg_1_received = LST_RADIO_MSG_NOT_RECEIVED;
+uint8_t lst_radio_msg_0_received = LST_RADIO_MSG_NOT_RECEIVED;
 
 /* Private variables ---------------------------------------------------------*/
 
@@ -29,5 +35,10 @@ void LST_Radio_Init(){
 * @brief Processes a byte from the Radio module
 */
 void LST_Radio_Process_Incoming_Byte(){
-
+	if(lst_uart_buffer_uart1[0]		   == LST_RADIO_MSG_5) 	lst_radio_msg_5_received = LST_RADIO_MSG_RECEIVED;
+	else if(lst_uart_buffer_uart1[0] == LST_RADIO_MSG_4)	lst_radio_msg_4_received = LST_RADIO_MSG_RECEIVED;
+	else if(lst_uart_buffer_uart1[0] == LST_RADIO_MSG_3)	lst_radio_msg_3_received = LST_RADIO_MSG_RECEIVED;
+	else if(lst_uart_buffer_uart1[0] == LST_RADIO_MSG_2)	lst_radio_msg_2_received = LST_RADIO_MSG_RECEIVED;
+	else if(lst_uart_buffer_uart1[0] == LST_RADIO_MSG_1)	lst_radio_msg_1_received = LST_RADIO_MSG_RECEIVED;
+	else if(lst_uart_buffer_uart1[0] == LST_RADIO_MSG_0)	lst_radio_msg_0_received = LST_RADIO_MSG_RECEIVED;
 }
