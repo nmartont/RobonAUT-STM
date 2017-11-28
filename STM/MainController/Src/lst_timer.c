9a59e1ea553cc66335f1dbf121e31e82aacea831
nmartont <nmarton.t@gmail.com>
2017-11-28 17:37:29 +0100

    asd

--- STM/MainController/Src/lst_timer.c
+++ STM/MainController/Src/lst_timer.c
@@ -8,7 +8,6 @@
 #include "lst_timer.h"
 
 /* Private variables ---------------------------------------------------------*/
-uint8_t cntr = 0;
 
 /******************************************************************************/
 /*                   Timer handling for RobonAUT 2018 Team LST                */
@@ -28,6 +27,5 @@ void LST_Timer_Init(){
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
-	HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_0);
-	cntr++;
+
 }
